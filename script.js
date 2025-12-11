5882cfe7f314475936787c3308b2913f65b4bb42
PetrHerbar <Herbiobio1@gmail.com>
2025-12-11 09:33:46 +0100

    script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const products = {
   },
   'lahvac': {
     name: 'Makarnika',
-    description: 'Krém na ruce s arnikou a makademiovým olejem vyživuje a zklidňuje pokožku rukou, zanechává je jemné a chráněné.',
+    description: 'Krém na ruce s arnikou a makadamovým olejem vyživuje a zklidňuje pokožku rukou, zanechává je jemné a chráněné.',
     price: '389 Kč',
     image: 'assets/Products/makarnika2.png',
     features: [
@@ -1084,6 +1084,7 @@ style.textContent = `
   }
 `;
 document.head.appendChild(style);
+
 
 
 
